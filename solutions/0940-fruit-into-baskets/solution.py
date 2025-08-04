5e573367e1c8b1900799752ca16dce8631d94c7e
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2025-08-04 15:20:20 +0000

    [LeetCode Sync] Runtime - 225 ms (87.18%), Memory - 16.9 MB (81.58%)

---
+++ solutions/0940-fruit-into-baskets/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        """
+        :type fruits: List[int]
+        :rtype: int
+        """
+        fruit_count = defaultdict(int)
+        left = 0
+        max_len = 0
+
+        for right in range(len(fruits)):
+            fruit_count[fruits[right]] += 1
+
+            while len(fruit_count) > 2:
+                fruit_count[fruits[left]] -= 1
+                if fruit_count[fruits[left]] == 0:
+                    del fruit_count[fruits[left]]
+                left += 1
+
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+        

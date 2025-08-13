8cc0cb77c03e276a1cd9aa37740913a604592ec8
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2025-08-13 17:27:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.3 MB (58.43%)

---
+++ solutions/0088-merge-sorted-array/solution.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i = m
+        j = n
+        k = m + n
+        while i>0 and j>0:
+            if(nums1[i-1]> nums2[j-1]):
+                nums1[k-1] = nums1[i-1]
+                i = i-1
+            else:
+                nums1[k-1] = nums2[j-1]
+                j = j-1
+            k = k-1
+
+        while j>0:
+            nums1[k-1] = nums2[j-1]
+            j = j-1
+            k = k-1
+

a1c8c1055b5a2ef913ab5dcc2457d044c5f2efa1
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2025-12-10 06:33:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.6 MB (24.66%)

---
+++ solutions/0048-rotate-image/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+
+        # fidn transpose
+        n = len(matrix)
+
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j] 
+        # revense the rows
+        for row in matrix:
+            row = row.reverse()
+        

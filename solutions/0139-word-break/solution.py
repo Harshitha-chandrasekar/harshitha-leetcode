bcd84bdef5b38940be20de91dff7d9e6ff0a03a0
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-06-09 10:14:47 +0000

    [LeetCode Sync] Runtime - 11 ms (16.96%), Memory - 12.6 MB (9.66%)

---
+++ solutions/0139-word-break/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        memo = {}
+        
+        def dp(i):
+            if i == len(s):
+                return True
+            if i in memo:
+                return memo[i]
+
+            for j in range(i,len(s)):
+                if s[i:j+1] in wordDict:
+                    if dp(j+1):
+                        memo[i] = True
+                        return True
+
+            memo[i] = False
+            return False
+
+        return dp(0)

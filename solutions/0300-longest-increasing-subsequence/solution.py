ae7002930aed35c3a7b47d26749a9cf0799c1115
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-06-09 10:23:56 +0000

    [LeetCode Sync] Runtime - 2222 ms (52.47%), Memory - 12.7 MB (34.14%)

---
+++ solutions/0300-longest-increasing-subsequence/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        cache = [1]*n
+
+        for i in range(n-1,-1,-1):
+            for j in range(i+1,n):
+                if nums[i]<nums[j]:
+                    cache[i] = max(cache[i],1+cache[j])
+
+        return max(cache)

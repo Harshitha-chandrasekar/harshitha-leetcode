92db62ad0bb9864b07483c46a738c314e1882c15
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-06-18 08:11:26 +0000

    [LeetCode Sync] Runtime - 7 ms (15.27%), Memory - 12.3 MB (55.55%)

---
+++ solutions/0202-happy-number/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = []
+        summ = 0
+        while summ not in seen:
+            while n>0:
+                summ = summ + math.pow(n%10,2)
+                n = n//10
+            if summ == 1:
+                return True
+            if summ in seen:
+                return False
+            seen.append(summ)
+            n = summ
+            summ = 0
+
+        return False

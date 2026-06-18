a250b15d012eb8263c566c56f9f744e5e15f69c5
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-06-18 03:40:27 +0000

    [LeetCode Sync] Runtime - 27 ms (5.36%), Memory - 15.9 MB (89.82%)

---
+++ solutions/0056-merge-intervals/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        i = 0
+        intervals.sort()
+        while i+1<len(intervals):
+            if intervals[i][1]<intervals[i+1][0]:
+                i = i+1
+            else:
+                intervals[i][0] = min(intervals[i][0],intervals[i+1][0])
+                intervals[i][1] = max(intervals[i][1],intervals[i+1][1])
+                intervals.pop(i+1)
+
+        return intervals

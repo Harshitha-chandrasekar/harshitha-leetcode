e0c53611a1065835a4c7e18efe105d8c54a19e08
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-06-22 15:23:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (88.45%)

--- solutions/0027-remove-element/solution.py
+++ solutions/0027-remove-element/solution.py
@@ -1,8 +1,8 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        k = 0  # Position for the next non-val element
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
         for i in range(len(nums)):
-            if nums[i] != val:
+            if nums[i]!=val:
                 nums[k] = nums[i]
-                k += 1
+                k = k+1
         return k

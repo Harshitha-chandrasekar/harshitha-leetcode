2e55ae13fa905511e92d4843cb8a81e2e7b1c4c3
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-06-27 07:08:14 +0000

    [LeetCode Sync] Runtime - 56 ms (18.75%), Memory - 25.3 MB (36.64%)

---
+++ solutions/0917-boats-to-save-people/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        boats = 0
+        l = 0
+        r = len(people)-1
+        while l<=r:
+            if people[l]+people[r]<=limit:
+                l = l+1
+                r = r-1
+            else:
+                r = r-1
+            boats = boats+1
+        return boats

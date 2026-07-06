8e12142528491ac091b55caec085b9497b411cc0
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-06 12:47:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (35.41%)

--- solutions/0021-merge-two-sorted-lists/solution.py
+++ solutions/0021-merge-two-sorted-lists/solution.py
@@ -1,26 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        dummy = ListNode()  # Dummy head node
-        current = dummy     # Pointer for building the new list
-
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        newn = ListNode()
+        h = newn
         while list1 and list2:
-            if list1.val < list2.val:
-                current.next = list1
+            if list1.val<=list2.val:
+                newn.next = list1
+                newn = newn.next
                 list1 = list1.next
             else:
-                current.next = list2
+                newn.next = list2
+                newn = newn.next
                 list2 = list2.next
-            current = current.next
 
-        # Attach the rest of whichever list is not empty
         if list1:
-            current.next = list1
-        else:
-            current.next = list2
+            newn.next = list1
+        if list2:
+            newn.next = list2
 
-        return dummy.next  # Skip dummy and return head of new list
+        return h.next

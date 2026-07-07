17921c1ecee63a4aa4a7938008e32441f6d55c64
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-07 06:57:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (65.51%)

--- solutions/0092-reverse-linked-list-ii/solution.py
+++ solutions/0092-reverse-linked-list-ii/solution.py
@@ -1,23 +1,21 @@
-class Solution(object):
-    def reverseBetween(self, head, left, right):
-        if not head or left == right:
-            return head
-                
-        dummy = ListNode(0)
-        dummy.next = head
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(0,head)
         prev = dummy
-        
-        
-        for _ in range(left - 1):
+
+        for _ in range(left-1):
             prev = prev.next
-            
         curr = prev.next
-        
-        for _ in range(right - left):
-            temp = curr.next
-            
-            curr.next = temp.next
-            temp.next = prev.next  
-            prev.next = temp   
-            
-        return dummy.next
+
+        for _ in range(right-left):
+            extract = curr.next
+            curr.next = extract.next
+            extract.next = prev.next
+            prev.next = extract
+
+        return dummy.next 

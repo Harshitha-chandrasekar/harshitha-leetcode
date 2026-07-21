cf409d884792778fd9a9973637045e17ed6b9ecc
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-21 08:36:23 +0000

    [LeetCode Sync] Runtime - 2 ms (8.54%), Memory - 19.4 MB (68.85%)

---
+++ solutions/0078-subsets/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def bt(i,subset):
+            res.append(subset.copy())
+
+            for j in range(i,len(nums)):
+                subset.append(nums[j])
+                bt(j+1,subset)
+                subset.pop()
+
+        bt(0,[])
+        return res

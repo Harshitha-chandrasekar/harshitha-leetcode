1c5c20d1f28632077107fd10ee669321f5af1cea
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-22 08:52:06 +0000

    [LeetCode Sync] Runtime - 110 ms (49.02%), Memory - 61.4 MB (55.02%)

---
+++ solutions/0077-combinations/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        nums = [i for i in range(1,n+1)]
+
+        def bt(i,sub):
+            if len(sub) == k:
+                res.append(sub.copy())
+                return
+
+            for j in range(i,len(nums)):
+                sub.append(nums[j])
+                bt(j+1,sub)
+                sub.pop()
+
+        bt(0,[])
+        return res

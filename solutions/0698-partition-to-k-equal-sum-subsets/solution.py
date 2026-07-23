516bca6c0a2d77ada4d3c4204e0c40d01dba4109
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-23 13:40:38 +0000

    [LeetCode Sync] Runtime - 33 ms (46.52%), Memory - 19.4 MB (80.44%)

---
+++ solutions/0698-partition-to-k-equal-sum-subsets/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total_len = sum(nums)
+        if total_len%k != 0:
+            return False
+
+        perlength = total_len/k
+        piles = [0]*k
+        nums.sort(reverse=True)
+
+        def dfs(i):
+            if i == len(nums):
+                return True
+
+            for j in range(k):
+                if piles[j] + nums[i] <= perlength:
+                    piles[j] += nums[i]
+                    if dfs(i+1):
+                        return True
+                    piles[j] -= nums[i]
+                    
+                if piles[j] == 0:
+                    break
+
+            return False
+
+        return dfs(0)

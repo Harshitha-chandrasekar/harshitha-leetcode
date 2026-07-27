42f365f5ba31eacd76e1b6306b101ebe32bbdc42
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-27 04:01:13 +0000

    [LeetCode Sync] Runtime - 3 ms (29.95%), Memory - 21.4 MB (58.06%)

--- solutions/0057-insert-interval/solution.py
+++ solutions/0057-insert-interval/solution.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         ans = []
         i = 0
         while i<len(intervals) and intervals[i][1]<newInterval[0]:

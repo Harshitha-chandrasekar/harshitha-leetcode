5fdc8596a465d5c665cf7b77e91665e34f7694ad
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-07-29 13:34:27 +0000

    [LeetCode Sync] Runtime - 7 ms (41.20%), Memory - 19.3 MB (20.80%)

--- solutions/0013-roman-to-integer/solution.py
+++ solutions/0013-roman-to-integer/solution.py
@@ -1,14 +1,13 @@
-class Solution(object):
-    def romanToInt(self, s):
-        l = len(s)
-        numeric = 0
-        roman_val ={'I':1,'V':5,"X":10,'L':50,'C':100,'D':500,'M':1000}
-        for i in range(0,l-1):
-            if roman_val[s[i]] < roman_val[s[i+1]]:
-                numeric = numeric - roman_val[s[i]]
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            "I": 1, "V": 5, "X": 10,
+            "L": 50, "C": 100, "D": 500, "M": 1000
+        }
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                res -= roman[s[i]]
             else:
-                numeric = numeric + roman_val[s[i]]
-        numeric = numeric + roman_val[s[l-1]]
-        return numeric
-                
-        
+                res += roman[s[i]]
+        return res

1baa936cb782f0b330d83e18dfe4caf6f3a2a81c
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-08-06 14:03:25 +0000

    [LeetCode Sync] Runtime - 115 ms (37.84%), Memory - 19.5 MB (94.41%)

--- solutions/0076-minimum-window-substring/solution.py
+++ solutions/0076-minimum-window-substring/solution.py
@@ -1,27 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t or not s:
-            return ""
-        count_t = {}
-        for c in t:
-            count_t[c] = count_t.get(c, 0) + 1
-        need = len(count_t)
-        have = 0
-        window_counts = {}
-        res = (float("inf"), -1, -1)
+        m,n = len(s),len(t)
+        count = {}
+        for c in t:#O(n)
+            if c in count:
+                count[c]+=1
+            else:
+                count[c]=1
+        minss = ""
+        minlen = float('inf')
         l = 0
-        for r in range(len(s)):
-            c = s[r]
-            window_counts[c] = window_counts.get(c, 0) + 1
-            if c in count_t and window_counts[c] == count_t[c]:
-                have += 1
-            while have == need:
-                if (r - l + 1) < res[0]:
-                    res = (r - l + 1, l, r)
-                left_char = s[l]
-                window_counts[left_char] -= 1
-                if left_char in count_t and window_counts[left_char] < count_t[left_char]:
-                    have -= 1
-                l += 1
-        window_len, start, end = res
-        return s[start : end + 1] if window_len != float("inf") else ""
+        tofind = n
+        for r in range(m):
+            if s[r] in count:
+                if count[s[r]]>0:
+                    tofind-=1
+                count[s[r]] -= 1
+            while tofind == 0:
+                if (r-l+1) < minlen:
+                    minlen = r-l+1
+                    minss = s[l:r+1]
+                if s[l] in count:
+                    count[s[l]] += 1
+                    if count[s[l]] > 0:
+                        tofind += 1
+                l+=1
+
+        return minss
+
+        

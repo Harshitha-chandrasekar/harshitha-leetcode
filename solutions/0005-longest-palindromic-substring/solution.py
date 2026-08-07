e8c12ae3b40c8597d9a0f9b7e5367d868a8a835e
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-08-07 13:46:00 +0000

    [LeetCode Sync] Runtime - 1979 ms (18.60%), Memory - 20.2 MB (5.06%)

--- solutions/0005-longest-palindromic-substring/solution.py
+++ solutions/0005-longest-palindromic-substring/solution.py
@@ -6,23 +6,20 @@ class Solution(object):
         """
         n = len(s)
         dp = [[False]*n for _ in range(n)]
-
-        start = 0
-        max_len = 1
         for i in range(n):
             dp[i][i] = True
 
+        maxl = 1
+        start = 0
         for i in range(n-1,-1,-1):
             for j in range(i+1,n):
                 if s[i] == s[j]:
                     if j-i<2 or dp[i+1][j-1]:
                         dp[i][j] = True
 
-
-                        curr = j-i+1
-                        if curr>max_len:
-                            max_len = curr
+                        if j-i+1>maxl:
+                            maxl = j-i+1
                             start = i
-
-        return s[start:start+max_len]  
         
+        return s[start:start+maxl]
+

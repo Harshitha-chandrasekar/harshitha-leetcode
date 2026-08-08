79d4de52cdd64978f032f310dd050ac74585cac6
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-08-08 06:45:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (20.37%)

--- solutions/0070-climbing-stairs/solution.py
+++ solutions/0070-climbing-stairs/solution.py
@@ -6,11 +6,10 @@ class Solution(object):
         """
         if n == 1:
             return 1
-        cache = [-1]*n
-        cache[0] = 1
-        cache[1] = 2
+        dp = [-1]*n
+        dp[0] = 1
+        dp[1] = 2
         for i in range(2,n):
-            cache[i] = cache[i-1] + cache[i-2]
+            dp[i] = dp[i-1] + dp[i-2]
 
-        return cache[n-1]
-        
+        return dp[n-1]

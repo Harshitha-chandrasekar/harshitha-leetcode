e3a21dd4d4089bd2228385941e6b8597c54f5244
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-08-08 08:31:06 +0000

    [LeetCode Sync] Runtime - 231 ms (86.40%), Memory - 21.6 MB (73.31%)

--- solutions/0200-number-of-islands/solution.py
+++ solutions/0200-number-of-islands/solution.py
@@ -2,20 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
 
-        def check(i,j):
-            if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j]!='1':
+        def bfs(i,j):
+            if i<0 or j<0 or i>=len(grid) or j>= len(grid[0]):
                 return
-
-            grid[i][j] = '.'
-            check(i+1,j)
-            check(i-1,j)
-            check(i,j+1)
-            check(i,j-1)
+            if grid[i][j] == '0':
+                return
+            
+            grid[i][j] = '0'
+            bfs(i+1,j)
+            bfs(i,j+1)
+            bfs(i-1,j)
+            bfs(i,j-1)
+            return
 
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
-                    check(i,j)
-                    count+=1
+                    count = count + 1
+                    bfs(i,j)
 
         return count

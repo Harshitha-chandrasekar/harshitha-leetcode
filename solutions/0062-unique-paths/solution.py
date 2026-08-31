68e0729d6e18c2eedda279f26566d3ec80d4c428
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-08-31 05:06:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (75.75%)

--- solutions/0062-unique-paths/solution.py
+++ solutions/0062-unique-paths/solution.py
@@ -1,19 +1,14 @@
-class Solution(object):
-    def uniquePaths(self, m, n):
-        """
-        :type m: int
-        :type n: int
-        :rtype: int
-        """
-        dp = [[0]*(n+1) for _ in range(m+1)]
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        arr = [[0]*(n+1) for _ in range(m+1)]
 
         for i in range(m):
-            dp[i][0] = 1
-        for j in range(n):
-            dp[0][j] = 1
+            arr[i][0] = 1
+        for i in range(n):
+            arr[0][i] = 1
 
         for i in range(1,m):
             for j in range(1,n):
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                arr[i][j] = arr[i][j-1] + arr[i-1][j]
 
-        return dp[m-1][n-1]
+        return arr[m-1][n-1]

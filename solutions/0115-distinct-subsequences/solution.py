799722d18e8579c4ce56f974bd29dfe3d1294f4e
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-09-02 12:36:46 +0000

    [LeetCode Sync] Runtime - 876 ms (7.84%), Memory - 254.1 MB (7.29%)

---
+++ solutions/0115-distinct-subsequences/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = {}
+        if len(t) > len(s):
+            return 0
+
+        def func(i,j):
+            if j == len(t):
+                return 1
+                
+            if i == len(s):
+                return 0
+            
+            if (i,j) in dp:
+                return dp[(i,j)]
+
+            res = func(i+1,j)
+            if s[i] == t[j]:
+                res += func(i+1,j+1)
+            dp[(i,j)] = res
+            return res
+
+        return func(0,0)

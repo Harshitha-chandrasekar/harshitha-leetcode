bea5368091f07bbdd97b9e0edb9cc9efb5c545b8
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-09-23 05:18:42 +0000

    [LeetCode Sync] Runtime - 21 ms (63.16%), Memory - 26 MB (69.13%)

--- solutions/0134-gas-station/solution.py
+++ solutions/0134-gas-station/solution.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
-        """
-        :type gas: List[int]
-        :type cost: List[int]
-        :rtype: int
-        """
-        n = len(gas)
-        i = 0
-        if sum(cost)>sum(gas):
+class Solution:
+    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
+        if sum(gas) < sum(cost):
             return -1
-        current_tank = 0
-        starting_station = 0
-        
+
+        total = 0
+        res = 0
         for i in range(len(gas)):
-            current_tank += gas[i] - cost[i]
-            
-            if current_tank < 0:
-                current_tank = 0
-                starting_station = i + 1
-                
-        return starting_station
+            total += (gas[i]-cost[i])
+
+            if total < 0:
+                total = 0
+                res = i+1
+
+        return res

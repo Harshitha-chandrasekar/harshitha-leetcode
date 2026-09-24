483b4c21190ca3d3c561ab670c1fab9430c87f6d
Harshitha Chandrasekar <136828773+Harshitha-chandrasekar@users.noreply.github.com>
2026-09-24 05:07:29 +0000

    [LeetCode Sync] Runtime - 32 ms (53.42%), Memory - 20.9 MB (82.18%)

--- solutions/0876-hand-of-straights/solution.py
+++ solutions/0876-hand-of-straights/solution.py
@@ -1,24 +1,20 @@
-class Solution(object):
-    def isNStraightHand(self, hand, groupSize):
-        """
-        :type hand: List[int]
-        :type groupSize: int
-        :rtype: bool
-        """
+class Solution:
+    def isNStraightHand(self, hand: list[int], groupSize: int) -> bool:
         n = len(hand)
         hand.sort()
         if n%groupSize!=0:
             return False
 
-        while hand:
-            c = hand[0]
-            i = 0
-            while i<groupSize:
-                if c in hand:
-                    hand.remove(c)
-                    c = c+1
-                    i = i+1
-                else:
-                    return False
-
+        count = Counter(hand)
+        for num in hand:
+            start = num
+            while count[start - 1]:
+                start -= 1
+            while start <= num:
+                while count[start]:
+                    for i in range(start, start + groupSize):
+                        if not count[i]:
+                            return False
+                        count[i] -= 1
+                start += 1
         return True
